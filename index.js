ddc23f7a0a7f2d36dd7127675c0f2650e16bb029
lilu <lilugirl2005@126.com>
2019-08-16 02:03:32 +0800

    2.2如何追踪变化

---
+++ index.js
@@ -0,0 +1,26 @@
+function defineReactive(data, key, val) {
+  Object.defineProperty(data, key, {
+    enumerable: true,
+    configurable: true,
+    get: function () {
+      console.log('get val', val);
+      return val;
+    },
+    set: function (newVal) {
+      console.log('set newVal', newVal);
+      if (val === newVal) {
+        return
+      }
+      val = newVal
+    }
+  })
+}
+
+
+var obj = {
+
+};
+
+defineReactive(obj, 'name', 'liuyi');
+
+var name = obj.name;
