1fe2940cce945b9df3d972d5f34c00723f2eec78
lilu <lilugirl2005@126.com>
2019-08-16 02:16:41 +0800

    2.4依赖收集在哪里

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 function defineReactive(data, key, val) {
+  let dep = [] //新增
   Object.defineProperty(data, key, {
     enumerable: true,
     configurable: true,
     get: function () {
       console.log('get val', val);
+      dep.push(window.target) // 新增
+      console.log('dep', dep);
       return val;
     },
     set: function (newVal) {
       console.log('set newVal', newVal);
       if (val === newVal) {
         return
+      }
+      // 新增
+      console.log('dep', dep);
+      for (let i = 0; i < dep.length; i++) {
+        // dep[i](newVal, val)
+
       }
       val = newVal
     }
@@ -23,4 +32,8 @@ var obj = {
 
 defineReactive(obj, 'name', 'liuyi');
 
+
 var name = obj.name;
+var name2 = obj.name;
+
+obj.name = 'xiao hei';
